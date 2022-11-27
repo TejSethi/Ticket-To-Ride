79cb3b57b2b273857608575ea69ab935a971267f
Daniel Jardine <118122650+DJ-public@users.noreply.github.com>
2022-11-27 12:40:33 -0500

    Removed Disorganised classes to move them to a new branch
    I will commit the disorganised classes to a new branch rather than the develop branch

--- Disorganised classes/Game.java
+++
@@ -1,4 +0,0 @@
-/**
- * The game class. Plays the game based on the State, calling methods as required and then updating when the state
- * becomes updated. The game is what will be referenced by the display.
- */
--- Disorganised classes/Settings.java
+++
@@ -1,7 +0,0 @@
-/**
- * Game settings class. Mainly for testing and debug reasons, not for player-changed settings.
- */
-
-public class Settings {
-    this.
-}
