a1b856f56732d2f3d9139c305cc71c4487cb1748
Daniel Jardine <118122650+DJ-public@users.noreply.github.com>
2022-11-27 12:47:59 -0500

    Re-added the disorganised classes
    I have put the disorganised classes as a folder in the src folder. I will put them into seperate packages soon.

---
+++ src/Disorganised classes/COLOR.java
@@ -0,0 +1,9 @@
+
+/**
+ * Color enum. For now there will only be 6 colors
+ */
+
+
+public enum COLOR {
+    RED, YELLOW, BLUE, PURPLE, WHITE, RAINBOW
+}
---
+++ src/Disorganised classes/TCard.java
@@ -0,0 +1,19 @@
+/**
+ * A Train car card.
+ */
+public class TCard extends Card{
+    private Color color;
+
+    public void TCard(String color) {
+        this.color = Color.color;
+    }
+    public void TCard(Color color) {
+        this.color = Color;
+    }
+    public Color getColor() {
+        return this.color;
+    }
+    public String getImage() {
+        return //TODO get an image from another file using a hashmap of color -> card.
+    }
+}
