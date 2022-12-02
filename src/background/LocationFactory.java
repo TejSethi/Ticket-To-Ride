dbf9fe05b90931e7368305721ed75ea7bd5aa5dd
Daniel Jardine <118122650+DJ-public@users.noreply.github.com>
2022-12-02 12:34:23 -0500

    Simplified LocationFactory
    I removed casting from LocationFactory.generate()

--- src/background/LocationFactory.java
+++ src/background/LocationFactory.java
@@ -10,13 +10,13 @@ import java.util.Set;
 public class LocationFactory {
     public static ArrayList<Location> generate(Game game) {
         ArrayList<Location> list = new ArrayList<>();
-        HashMap<String, int[]> setup = game.set.locations;
-        Object[] keys = setup.keySet().toArray();
-        Object[] vals = setup.values().toArray();
+        HashMap<String, int[]> setup = Settings.locations;
+        String[] keys = setup.keySet().toArray(new String[setup.size()]);
+        int[][] vals = setup.values().toArray(new int[setup.size()][2]);
         for (int i = 0; i < setup.size(); i++) {
-            int x = ((int[]) vals[i])[0];
-            int y = ((int[]) vals[i])[1];
-            String name = (String) keys[i];
+            int x = vals[i][0];
+            int y = vals[i][1];
+            String name = keys[i];
             list.add(new Location(x, y, name));
         }
         return list;
