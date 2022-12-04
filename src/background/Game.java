636495db35c7642462802bf7a1a8cdb41be290e6
Daniel Jardine <118122650+DJ-public@users.noreply.github.com>
2022-12-04 14:56:30 -0500

    Updated Settings.java
    I cleaned up Settings.java, as well as added Trainpaths.csv, and the corresponding variable in Settings.java.
    I also added to Game.java, which now calls LocationFactory when initialized.

--- src/background/Game.java
+++ src/background/Game.java
@@ -1,4 +1,11 @@
 package background;
+
+import boardobjects.Location;
+import boardobjects.Trainpath;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * The game class. Plays the game based on the State, calling methods as required and then updating when the state
  * becomes updated. The game is what will be referenced by the display.
@@ -6,7 +13,28 @@ package background;
 
 public class Game {
     public Settings set;
+    private HashMap<String, Location> locationMap;
+    private ArrayList<Trainpath> allPaths;
     public Game() {
         this.set = new Settings();
+        setupBoard();
+    }
+    public ArrayList<Location> getLocations(String[] locationNames) {
+        ArrayList<Location> output = new ArrayList<>();
+        for (int i = 0; i < locationNames.length; i++) {
+            output.add(locationMap.get(locationNames[i]));
+        }
+        return output;
+    }
+    private void setupBoard() {
+        // Setup locationMap
+        ArrayList<Location> locations = LocationFactory.generate(this);
+        String name;
+        for (int i = 0; i < locations.size(); i++) {
+            name = locations.get(i).getName();
+            this.locationMap.put(name, locations.get(i));
+        }
+        // Generate Paths
+        //this.allPaths = TrainpathFactory.generate();  //TODO TrainpathFactory is still in progress. When done uncomment
     }
 }
--- src/background/Settings.java
+++ src/background/Settings.java
@@ -2,7 +2,6 @@ package background;
 
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,13 +18,29 @@ public class Settings {
     public static int numCardsEachColor;
     public static int numCardsRainbow;
     public static HashMap<String, int[]> locations;
+    public static ArrayList<String[]> trainpaths;
 
     /**
      * Constructor.
      */
     public Settings() {
+        // Set numCardsEachColor
         numCardsEachColor = 15;
+        // Set numCardsRainbow
         numCardsRainbow = 5;
+        // Set locations variable
+            // First get LocationArr (an array of locations)
+        ArrayList<int[]> locationArr = getCoords();
+            // Then get location names, putting <name, location coordinates> into the locations hashmap
+        String[] spl = getNames();
+        for (int i = 0; i < spl.length; i++) {
+            locations.put(spl[i], locationArr.get(i));
+        }
+        // Set trainPaths
+        trainpaths = getTrainPaths();
+    }
+
+    private ArrayList<int[]> getCoords() {
         Pattern isNumber = Pattern.compile("[0-9]+");
         Matcher matcher;
         ArrayList<int[]> locationArr = new ArrayList<>();
@@ -58,16 +73,53 @@ public class Settings {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return locationArr;
+    }
+
+    private String[] getNames() {
+        String[] spl = {};
         try {
             File file = new File("src/background/LocationNames.csv");
             Scanner sc = new Scanner(file);
             String line = sc.nextLine();
-            String[] spl = line.strip().split(",");
-            for (int i = 0; i < spl.length; i++) {
-                locations.put(spl[i], locationArr.get(i));
-            }
+            spl = line.strip().split(",");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[]{"error"};
+        }
+        return spl;
+    }
+
+    private ArrayList<String[]> getTrainPaths() {
+        ArrayList<String[]> pathArr = new ArrayList<>();
+        Pattern isQuoted = Pattern.compile("(\")(.+)(\")");
+        Matcher matcher;
+        String[] spl = {};
+        try {
+            File file = new File("src/background/Trainpaths.csv");
+            Scanner sc = new Scanner(file);
+            String line = sc.nextLine();
+            spl = line.strip().split(",");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        int j = 0;
+        String[] path = new String[2];
+        for (int i = 0; i < spl.length; i++) {
+            if (j == 2) {
+                pathArr.add(path);
+                path = new String[2];
+                j = 0;
+            }
+            matcher = isQuoted.matcher(spl[i]);
+            if (matcher.find()) {
+                path[j] = matcher.group(2);
+                j++;
+            }
+        }
+        if (j == 2) {
+            pathArr.add(path);
+        }
+        return pathArr;
     }
 }
